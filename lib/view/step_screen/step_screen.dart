7bdae55f356c315790652b38ae10384884fe29d2
Muntasir Ashif <muntasirashifee@gmail.com>
2025-03-14 12:56:14 +0600

    add gesture ditector in primary button

--- lib/view/step_screen/step_screen.dart
+++ lib/view/step_screen/step_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:ovella_period_tracker_app/utility/utils.dart';
 
 class StepScreen extends StatelessWidget {
   const StepScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Utils.backButton(context),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
