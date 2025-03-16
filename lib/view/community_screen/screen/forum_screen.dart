d3dca069eb0673e81e9a1fa284ba39e5cd4e2a2a
Safrid Bhueyan <safridbhueyan>
2025-03-16 19:29:17 +0600

    pull

---
+++ lib/view/community_screen/screen/forum_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ovella_period_tracker_app/model/catagoryModel.dart';
+import 'package:ovella_period_tracker_app/routing/route_name.dart';
+import 'package:ovella_period_tracker_app/view/community_screen/widget/categoryList.dart';
+import 'package:ovella_period_tracker_app/view/community_screen/widget/listviewbuilder.dart';
+
+class ForumScreen extends StatelessWidget {
+  const ForumScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CategorySection(
+            title: "Menstrual Health",
+            categories: categories1,
+            routeName: RouteName.menstrualScreen,
+          ),
+          CategorySection(
+            title: "Fertility",
+            categories: categories2,
+            routeName: RouteName.fertilityScreen,
+          ),
+          CategorySection(
+            title: "Sexual Health",
+            categories: categories3,
+            routeName: RouteName.sexualScreen,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//------------Category Section Widget--------------
+class CategorySection extends StatelessWidget {
+  final String title;
+  final List<CategoryModel> categories;
+  final String routeName;
+
+  const CategorySection({
+    Key? key,
+    required this.title,
+    required this.categories,
+    required this.routeName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(title, style: Theme.of(context).textTheme.bodyLarge),
+            Spacer(),
+            GestureDetector(
+              onTap: () => Navigator.pushNamed(context, routeName),
+              child: Text(
+                "See all",
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xff1E1E1E),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 16.h),
+        SizedBox(
+          height: 270.h,
+          child: CategoryList(right: 12.w, categories: categories),
+        ),
+        SizedBox(height: 24.h),
+      ],
+    );
+  }
+}
