0a056dc728b8f17d44cbabf04b0afafe15cbd591
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-03-19 13:04:41 +0600

    demo ai chat added

--- lib/view/ai_chat/widgets/chat_input_field.dart
+++ lib/view/ai_chat/widgets/chat_input_field.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ovella_period_tracker_app/model/chat_model.dart';
+import 'package:ovella_period_tracker_app/utility/utils.dart';
+import 'package:ovella_period_tracker_app/view_model/chat_screen_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../../../constant/images.dart';
 
@@ -19,13 +23,30 @@ class ChatInputField extends StatelessWidget{
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 6.w,vertical: 6.h),
               decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    offset: Offset(0, 8),
+                    blurRadius: 7.r,
+                    spreadRadius: 7.r,
+                  )
+                ],
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(150.r),
               ),
               child: TextFormField(
+                controller: context.read<ChatScreenProvider>().chatController,
                 onTapOutside: (_){
                   FocusScope.of(context).unfocus();
                 },
+                // onEditingComplete: (){
+                //   context.read<ChatScreenProvider>().sendCommand();
+                // },
+                onFieldSubmitted: (_){
+
+                  context.read<ChatScreenProvider>().sendCommand();
+                },
+                style: Theme.of(context).textTheme.bodyMedium,
                 decoration: InputDecoration(
                     enabledBorder: InputBorder.none,
                     focusedBorder: InputBorder.none,
@@ -39,7 +60,9 @@ class ChatInputField extends StatelessWidget{
                         color: Color(0xff676364)
                     ),
                     suffixIcon: GestureDetector(
-                      onTap: (){},
+                      onTap: (){
+                        context.read<ChatScreenProvider>().sendCommand();
+                      },
                       child: Container(
                         padding: EdgeInsets.all(12.r),
                         decoration: BoxDecoration(
@@ -62,9 +85,17 @@ class ChatInputField extends StatelessWidget{
           GestureDetector(
               child: Container(
                   padding: EdgeInsets.all(14.r),
-                  decoration: const BoxDecoration(
+                  decoration:  BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        offset: Offset(0, 8),
+                        blurRadius: 7.r,
+                        spreadRadius: 7.r,
+                      )
+                    ],
                   ),
                   child : Image.asset(AppImages.microphone,
                     width: 24.w,
