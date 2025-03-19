ef13209994813855ecc8c1633675794ce7794ad7
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-03-19 09:17:52 +0600

    chat screen provider added

--- lib/view/ai_chat/chat_screen.dart
+++ lib/view/ai_chat/chat_screen.dart
@@ -5,6 +5,7 @@ import 'package:ovella_period_tracker_app/constant/images.dart';
 import 'package:ovella_period_tracker_app/constant/padding.dart';
 import 'package:ovella_period_tracker_app/theme/theme/theme_extensions/color_palette.dart';
 import 'package:ovella_period_tracker_app/utility/utils.dart';
+import 'package:ovella_period_tracker_app/view/ai_chat/widgets/chat_input_field.dart';
 import 'package:ovella_period_tracker_app/widgets/background_widget.dart';
 
 class ChatScreen extends StatelessWidget{
@@ -19,6 +20,8 @@ class ChatScreen extends StatelessWidget{
             children: [
               Column(
                 children: [
+
+                  /// Appbar
                   AppBar(
                     centerTitle: true,
                     actions: [
@@ -35,77 +38,19 @@ class ChatScreen extends StatelessWidget{
                       size: 20.r,)
                     ),
                   ),
-                  Column(
-                    children: [
 
-                    ],
+                  Padding(
+                    padding: AppPadding.screenVerticalPadding,
+                    child: Column(
+                      children: [
+
+                      ],
+                    ),
                   )
                 ],
               ),
 
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 150.h,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 6.w,vertical: 6.h),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(150.r),
-                        ),
-                        child: TextFormField(
-                          onTapOutside: (_){
-                            FocusScope.of(context).unfocus();
-                          },
-                          decoration: InputDecoration(
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            border: InputBorder.none,
-                            fillColor: Colors.transparent,
-                            hintText: "Type your question",
-                            suffixIcon: GestureDetector(
-                              onTap: (){},
-                              child: Container(
-                                padding: EdgeInsets.all(12.r),
-                                decoration: BoxDecoration(
-                                  color: Color(0xffF4F6F6),
-                                  shape: BoxShape.circle
-                                ),
-                                child: Image.asset(AppImages.sendIcon,
-                                width: 16.w,
-                                height: 16.h,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            )
-                          ),
-                        ),
-
-                      ),
-                    ),
-                    SizedBox(width: 12.w,),
-                    GestureDetector(
-                        child: Container(
-                          padding: EdgeInsets.all(14.r),
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            child : Image.asset(AppImages.microphone,
-                            width: 24.w,
-                              height: 24.h,
-                              fit: BoxFit.cover,
-                            )
-                        )
-                    )
-                  ],
-                ),
-              )
+              ChatInputField(),
             ],
           ),
         )
