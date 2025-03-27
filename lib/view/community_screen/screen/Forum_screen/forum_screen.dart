491117447af0a8827f1f458fe87b7d177e2b8ccb
df <fd>
2025-03-27 14:41:17 +0600

    localization

--- lib/view/community_screen/screen/Forum_screen/forum_screen.dart
+++ lib/view/community_screen/screen/Forum_screen/forum_screen.dart
@@ -5,28 +5,31 @@ import 'package:ovella_period_tracker_app/model/catagoryModel.dart';
 import 'package:ovella_period_tracker_app/routing/route_name.dart';
 import 'package:ovella_period_tracker_app/view/community_screen/widget/categoryList.dart';
 import 'package:ovella_period_tracker_app/view/community_screen/widget/listviewbuilder.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ForumScreen extends StatelessWidget {
   const ForumScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+        final appLocalization = AppLocalizations.of(context);
+
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CategorySection(
-            title: "Menstrual Health",
+            title: appLocalization!.menstrualhealth ?? "Menstrual Health",
             categories: categories1,
             routeName: RouteName.menstralScreen,
           ),
           CategorySection(
-            title: "Fertility",
+            title:appLocalization!.fertility ?? "Fertility",
             categories: categories2,
             routeName: RouteName.fertilityScreen,
           ),
           CategorySection(
-            title: "Sexual Health",
+            title:appLocalization!.sexualhealth ?? "Sexual Health",
             categories: categories3,
             routeName: RouteName.sexualScreen,
           ),
@@ -52,6 +55,8 @@ class CategorySection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+            final appLocalization = AppLocalizations.of(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -65,7 +70,7 @@ class CategorySection extends StatelessWidget {
               GestureDetector(
                 onTap: () => {Navigator.pushNamed(context, routeName)},
                 child: Text(
-                  "See all",
+                appLocalization!.seeall ??  "See all",
                   style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                     fontSize: 14.sp,
                     fontWeight: FontWeight.w500,
