fe3ee95804ebe91e9f8a2c7335f4c7d4c3f5f27f
df <fd>
2025-03-27 09:43:03 +0600

    localization

--- lib/view/community_screen/screen/event_screen/eventDetails_screen.dart
+++ lib/view/community_screen/screen/event_screen/eventDetails_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ovella_period_tracker_app/constant/padding.dart'
